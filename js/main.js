4aff789e57ef439a075e7d197db0a2f3456f6950
Oleg Yarovik <65862979+yarovik1982@users.noreply.github.com>
2021-04-08 20:03:26 +0300

    Add files via upload
    что-то изменил, все работает

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const swiper_1 = new Swiper('.channel-slider__first', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 6,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.channel__first-button-next',
+      prevEl: '.channel__first-button-prev',
+    },
+  });
+  const swiper_2 = new Swiper('.channel-slider__second', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 3,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.channel__second-button-next',
+      prevEl: '.channel__second-button-prev',
+    },
+  });
+  const swiper_3 = new Swiper('.channel-slider__third', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 6,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.channel__third-button-next',
+      prevEl: '.channel__third-button-prev',
+    },
+  });
+ 
