15c246b60a9b55c3193eee06f457033313a55621
Oleg Yarovik <65862979+yarovik1982@users.noreply.github.com>
2021-04-10 12:57:54 +0300

    Add files via upload
    все пофиксил

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 const swiper_1 = new Swiper('.channel-slider__first', {
     // Optional parameters
     loop: true,
-    slidesPerView: 6,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints:{
+      1990:{
+        slidesPerView: 6,
+      },
+      1600:{
+        slidesPerView:5,
+      },
+      1300:{
+        slidesPerView:4,
+      },
+      1100:{
+        slidesPerView:3,
+      },
+      800:{
+        slidesPerView: 1,
+      },
+    },
     // Navigation arrows
     navigation: {
       nextEl: '.channel__first-button-next',
@@ -11,7 +29,16 @@ const swiper_1 = new Swiper('.channel-slider__first', {
   const swiper_2 = new Swiper('.channel-slider__second', {
     // Optional parameters
     loop: true,
-    slidesPerView: 3,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints:{
+      1600:{
+        slidesPerView: 3,
+      },
+      1100:{
+        slidesPerView: 2,
+      },
+    },
     // Navigation arrows
     navigation: {
       nextEl: '.channel__second-button-next',
@@ -21,11 +48,53 @@ const swiper_1 = new Swiper('.channel-slider__first', {
   const swiper_3 = new Swiper('.channel-slider__third', {
     // Optional parameters
     loop: true,
-    slidesPerView: 6,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints:{
+      1990:{
+        slidesPerView: 6,
+      },
+      1600:{
+        slidesPerView:5,
+      },
+      1300:{
+        slidesPerView:4,
+      },
+      1100:{
+        slidesPerView:3,
+      },
+      800:{
+        slidesPerView: 1,
+      },
+    },
     // Navigation arrows
     navigation: {
       nextEl: '.channel__third-button-next',
       prevEl: '.channel__third-button-prev',
     },
   });
- 
+
+
+
+  if (document.documentElement.scrollWidth <= 640) {
+    swiper_1.destroy();
+    swiper_2.destroy();
+    swiper_3.destroy();
+  }
+
+  function showSearch(){ 
+  const searchBtn = document.querySelector('.mobile-search');
+  const mobileSearch = document.querySelector('.input-group');
+  const buttonSearch = document.querySelector('.button-search');
+
+  buttonSearch.addEventListener('click', () => {
+    mobileSearch.classList.remove('is-open');
+    searchBtn.style.opacity = '1';
+  })
+
+  searchBtn.addEventListener('click', ()=>{
+    mobileSearch.classList.add('is-open');
+    searchBtn.style.opacity = '0';
+  })
+}
+showSearch();
